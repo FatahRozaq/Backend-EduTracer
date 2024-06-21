251f8ecd44fd0193728feee23e44ff8c5d348e04
FatahRozaq <fatah.rozaq20@gmail.com>
2024-06-22 06:00:23 +0700

    Update API tugas

--- app/Http/Controllers/TugasController.php
+++ app/Http/Controllers/TugasController.php
@@ -9,8 +9,48 @@ class TugasController extends Controller
 {
     public function index()
     {
-        // return Tugas::all();
         return response()->json(['data' => Tugas::orderBy('id_tugas', 'ASC')->get()]);
-        
+    }
+
+    public function show($id)
+    {
+        return Tugas::findOrFail($id);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nama_tugas' => 'required',
+            'deskripsi' => 'required',
+            'status' => 'required',
+        ]);
+
+        $tugas = Tugas::create($validatedData);
+
+        return response()->json($tugas, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $tugas = Tugas::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'nama_tugas' => 'required',
+            'deskripsi' => 'required',
+            'tenggat_tugas' => 'required|date',
+            'status' => 'required',
+        ]);
+
+        $tugas->update($validatedData);
+
+        return response()->json($tugas, 200);
+    }
+
+    public function destroy($id)
+    {
+        $tugas = Tugas::findOrFail($id);
+        $tugas->delete();
+
+        return response()->json("Data berhasil dihapus", 204);
     }
 }
